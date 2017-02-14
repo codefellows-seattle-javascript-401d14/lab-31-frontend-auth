b44d9fba2d2b33f05611e752d254a9e7b89f1b2c
Geno Guerrero <geno.guerrero@gmail.com>
2017-02-13 20:21:36 -0800

    lab-gnoevil

--- app/service/auth-service.js
+++ app/service/auth-service.js
@@ -13,7 +13,22 @@ require('angular').module('demoApp')
         Accept: 'application/json',
       },
     };
-
+    authService.login = function(user){
+      let url = `${__API_URL__}/api/login`;
+      let base64 = $window.btoa(`${user.username}:${user.password}`);
+      let config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Basic ${base64}`,
+        },
+      };
+      return $http.get(url, config)
+      .then(res => {
+        $log.log('success');
+        return res.data;
+      });
+    };
     return $http.post(url, user, config)
     .then(res => {
       $log.log('succecss');

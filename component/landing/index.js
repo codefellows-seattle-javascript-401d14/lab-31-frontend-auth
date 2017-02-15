031c21b3eb56dfaee8f4e9c859956e6b96323911
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2017-02-15 13:15:41 -0800

    code

---
+++ component/landing/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const angular = require ('angular');
+
+angular.module('myApp')
+.component('landing', {
+  template:
+
+  `<div>
+    <h1> Welcome to the Home page  </h1>
+    <signup>  </signup>
+    <login>    </login>
+
+    </div>`,
+
+});

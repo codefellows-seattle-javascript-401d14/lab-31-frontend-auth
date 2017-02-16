b5d84ed8df87245d0659aabe2552b05b56f047aa
Geno Guerrero <geno.guerrero@gmail.com>
2017-02-16 12:09:51 -0800

    added galleryService

--- app/service/gallery-service.js
+++ app/service/gallery-service.js
@@ -2,7 +2,7 @@
 
 require('angular').module('demoApp')
 .service('galleryService', ['$log', '$http', 'authService', function($log, $http, authService) {
-  let galleryService = {},
+  let galleryService = {};
   galleryService.create = gallery => {
     let url = `${__API_URL__}/api/gallery`;
     let config = {
@@ -22,8 +22,46 @@ require('angular').module('demoApp')
     })
   };
   galleryService.fetchAll = () => {
-    
-  },
-};
-
-])
+    let url = `${__API_URL__}/api/gallery`;
+    let config = {
+      headers: {
+        Accept: 'application/json',
+      },
+    };
+    return authService.fetchToken()
+    .then(token => {
+      config.headers.Authorization = `Bearer ${token}`;
+      return $http.get(url, config);
+    })
+    .then(res => {
+      $log.log('post /api/gallery success');
+      return res.data;
+    })
+  };
+  galleryService.delete = gallery => {
+    let url = `${__API_URL__}/api/gallery/${gallery._id}`;
+    let config = {
+      headers: {
+        Accept: 'application/json',
+      },
+    };
+    galleryService.update = gallery => {
+      let url = `{__API_URL__}/api/gallery/${gallery._id}`;
+      let config = {
+        headers: {
+          'Content-Type': 'application/jspn',
+          Accept: 'application/json',
+        },
+      };
+      return authService.fetchToken()
+      .then(token => {
+        config.headers.Authorization = `Bearer ${token}`;
+        return $http.put(url, gallery, config);
+      })
+      .then(res => {
+        $log.log('post /api/gallery success');
+        return res.data;
+      })
+    };
+    return galleryService;
+  }]);

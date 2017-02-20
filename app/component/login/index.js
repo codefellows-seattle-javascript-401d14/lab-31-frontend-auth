93d89640ed15c6bbb931a22958c5b86f8265972c
Stephen Anderson <sa3196a@student.american.edu>
2017-02-19 19:10:35 -0800

    added login

---
+++ app/component/login/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+require('angular').module('demoApp')
+.component('login', {
+  template: require('./login.html'),
+  controllerAs: 'loginCtrl',
+  controller: [ '$log', 'authService', function($log, authService){
+    this.$onInit = () => {
+      this.user = {username: '', password: ''};
+      this.handleSubmit = () => {
+        authService.login(this.user)
+        .then(token => {
+          alert(`token ${token}`);
+          this.user = {username: '', password: ''};
+        })
+        .catch('could not login');
+      };
+    };
+  }],
+});
